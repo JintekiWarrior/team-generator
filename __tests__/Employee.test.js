5407eb4622465b88b865d14022a4327dd439db60
JintekiWarrior <jintekiwarrior@gmail.com>
2022-03-29 21:44:21 -0400

    Add employee class constructor - Add tests for employee class constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -13,3 +13,21 @@ test('gets name of employee', () => {
 
   expect(employee.getName()).toBe('Moe')
 })
+
+test('gets the id of the employee', () => {
+  const employee = new Employee('Moe', '3774775', 'moe@moe.com')
+
+  expect(employee.getId()).toBe('3774775')
+})
+
+test('gets the email of the employee', () => {
+  const employee = new Employee('Moe', '3774775', 'moe@moe.com')
+
+  expect(employee.getEmail()).toBe('moe@moe.com')
+})
+
+test('gets the role of the employee', () => {
+  const employee = new Employee('Moe', '3774775', 'moe@moe.com')
+
+  expect(employee.getRole()).toBe('Employee')
+})
